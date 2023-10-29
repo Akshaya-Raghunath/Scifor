651d0b28a806c723816612bcf4cb761f414787c0
Akshaya-Raghunath <136258953+Akshaya-Raghunath@users.noreply.github.com>
2023-10-30 01:24:24 +0530

    Create baseball game.py

---
+++ Assignment3/simulation/baseball game.py
@@ -0,0 +1,29 @@
+You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x.
+Record a new score of x.
+'+'.
+Record a new score that is the sum of the previous two scores.
+'D'.
+Record a new score that is the double of the previous score.
+'C'.
+Invalidate the previous score, removing it from the record.
+Return the sum of all the scores on the record after applying all the operations.
+
+The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid
+
+class Solution(object):
+    def calPoints(self, operations):
+        x=[]
+        for i in operations:
+            if i == "+":
+                x.append(x[-1]+x[-2])
+            elif i == "D":
+                x.append(2*x[-1])
+            elif i == "C":
+                x.pop()
+            else:
+                x.append(int(i))
+        return sum(x)
