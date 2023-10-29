a9f9055ad032923ae2932bf4666d4a35dcb4fb30
Akshaya-Raghunath <136258953+Akshaya-Raghunath@users.noreply.github.com>
2023-10-30 01:16:37 +0530

    Create arithmetic progression from sequence.py

---
+++ Assignment3/Basic Implementation/arithmetic progression from sequence.py
@@ -0,0 +1,14 @@
+A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+
+Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
+
+class Solution(object):
+    def canMakeArithmeticProgression(self, arr):
+        arr.sort()
+        d = arr[1] - arr[0]
+        for i in range(1, len(arr) - 1):
+            if arr[i + 1] - arr[i] != d:
+                return False
+        return True
+    arr = [3, 5, 1]
+    print(arr)
