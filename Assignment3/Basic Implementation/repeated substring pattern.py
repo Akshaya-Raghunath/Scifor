68abad5ea258b623424af0cd4b786d5fcfc85ab8
Akshaya-Raghunath <136258953+Akshaya-Raghunath@users.noreply.github.com>
2023-10-30 01:09:53 +0530

    Create repeated substring pattern.py

---
+++ Assignment3/Basic Implementation/repeated substring pattern.py
@@ -0,0 +1,11 @@
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together
+
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        length = len(s)
+        for i in range(1, length // 2 + 1):
+            if length % i == 0:
+                substring = s[:i]
+                if substring * (length // i) == s:
+                    return True
+        return False        
