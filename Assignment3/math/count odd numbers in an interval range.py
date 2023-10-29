dc986e97556eb2349f75586bf5f4ca93ae352c41
Akshaya-Raghunath <136258953+Akshaya-Raghunath@users.noreply.github.com>
2023-10-30 01:33:55 +0530

    Create count odd numbers in an interval range.py

---
+++ Assignment3/math/count odd numbers in an interval range.py
@@ -0,0 +1,8 @@
+Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive)
+
+lass Solution(object):
+    def countOdds(self, low, high):
+        count = (high-low)//2
+        if high % 2 != 0 or low % 2 != 0:
+            count += 1
+        return count  
